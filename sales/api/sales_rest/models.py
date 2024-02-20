4f2aa4833853efac0de3508c3a572442c0aab848
joehands0me <johansonbomba@gmail.com>
2024-02-19 21:31:22 -0800

    Started sales, created models for products and customers

---
+++ sales/api/sales_rest/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from address.models import AddressField
+
+
+class ProductVO(models.Model):
+    import_href = models.CharField(max_length=100, unique=True)
+    item_number = models.PositiveSmallIntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    unit_price = models.PositiveSmallIntegerField()
+    stock = models.PositiveSmallIntegerField()
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    company = models.CharField(max_length=50)
+    contact_number = PhoneNumberField()
+    address = AddressField(related_name="billing_address")
+    shipping_address = AddressField(related_name="shipping_address")
